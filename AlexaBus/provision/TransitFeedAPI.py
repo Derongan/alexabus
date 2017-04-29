8238bd9e31e0bf49b32a527dd14a7fc6e0cad975
David Friedman <dgfriedman@ucdavis.edu>
2017-04-29 13:33:04 -0700

    Add function to get all feed ids and populated the loader

--- AlexaBus/provision/TransitFeedAPI.py
+++ AlexaBus/provision/TransitFeedAPI.py
@@ -10,13 +10,33 @@ class TransitFeedAPI(object):
         return requests.get("https://api.transitfeeds.com/v1/getLocations", params={"key": self.key},
                             timeout=self.timeout).json()
 
-    def get_feed_ids(self, location_id):
+    def get_feed_ids(self, location_id, descendants=0, type="gtfs", page=1):
         return requests.get("https://api.transitfeeds.com/v1/getFeeds", params={
-            "key": self.key, "location": location_id, "descendants": 0, "page": 1,
-            "limit": 0
+            "key": self.key, "location": location_id, "descendants": descendants, "page": page,
+            "limit": 100, "type": type
         }).json()
+
+    def get_all_feed_ids(self):
+        first = self.get_feed_ids(0, 0, page=0)['results']
+        print first.keys()
+        num_pages = first['numPages']
+
+        res = [feed['id'] for feed in first['feeds']]
+
+        for i in range(1, num_pages):
+            nxt = self.get_feed_ids(0, 0, page=i)['results']['feeds']
+
+            res += [feed['id'] for feed in nxt]
+
+        return res
 
     def get_feed_zip_url(self, feed_id):
         return requests.get("https://api.transitfeeds.com/v1/getLatestFeedVersion", params={
             "key": self.key, "feed": feed_id
         }, allow_redirects=False).url
+
+
+if __name__ == "__main__":
+    from config import TRANSIT_FEED_API_KEY
+    x = TransitFeedAPI(TRANSIT_FEED_API_KEY)
+    print x.get_all_feed_ids()
---
+++ AlexaBus/provision/db_load.py
@@ -0,0 +1,28 @@
+from pygtfsdb.GtfsDb import GtfsDb
+from TransitFeedAPI import TransitFeedAPI
+from config import *
+import traceback
+
+if __name__ == "__main__":
+    tapi = TransitFeedAPI(TRANSIT_FEED_API_KEY)
+
+    db_url = "postgresql://{user}:{password}@{endpoint}:{port}/{db}".format(endpoint=DB_ENDPOINT, db=DB_NAME,
+                                                                            user=DB_USER,
+                                                                            password=DB_PASSWORD, port=DB_PORT)
+    gtdb = GtfsDb(db_url)
+
+    feed_ids = tapi.get_all_feed_ids()
+
+    for fid in feed_ids:
+        try:
+            url = tapi.get_feed_zip_url(fid)
+        except:
+            print "Problem getting url for feed {0}".format(fid)
+            continue
+
+        try:
+            gtdb.load(url, fid)
+        except Exception, e:
+            print "Problem loading for {0}".format(fid)
+            print traceback.format_exc()
+            continue
